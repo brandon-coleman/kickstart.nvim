f357310f3cd4f53ec9cfc28d63297a55e6b6fa7c
Brandon Coleman <brandoncoleman@beyondfinance.com>
2024-09-18 14:27:55 -0400

    Update copilot settings

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,9 @@
 return {
-  "github/copilot.vim"
+  'github/copilot.vim',
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_disable_tab = true
+    vim.keymap.set('i', '<C-Space>', 'copilot#Accept("\\<CR>")')
+  end,
 }
